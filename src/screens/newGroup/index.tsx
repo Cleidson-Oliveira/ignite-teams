4cdb3e33d473227d4855d984360952a225d8f7da
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-11-04 13:37:48 -0300

    chore: add screen to register new group

---
+++ src/screens/newGroup/index.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@components/button";
+import { Header } from "@components/header";
+import { Highlight } from "@components/highlight";
+import Input from "@components/input";
+import { Conteiner, Content, Icon } from "./style";
+
+function NewGroup () {
+    return (
+        <Conteiner>
+            <Header showBackButton/>
+            <Content>
+                <Icon />
+                <Highlight
+                    title="Nova turma"
+                    subtitle="Crie a turma para adicionar as pessoas"
+                />
+                <Input placeholder="Nome da turma" style={{marginBottom: 20}}/>
+                <Button name="Criar"/>
+            </Content>
+        </Conteiner>
+    );
+}
+
+export default NewGroup;
